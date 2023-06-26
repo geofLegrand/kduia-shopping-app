ec9768fab5aea6563983fe0b34a1a414ffda493d
Kossi KOTOKLO <gessongue@gmail.com>
2023-06-26 20:10:38 +0100

    implement AppContext component and CartValue compo
    Signed-off-by: Kossi KOTOKLO <gessongue@gmail.com>

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -2,6 +2,16 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const {Location,expenses} = useContext(AppContext)
+
+    const totalExpenses = expenses.reduce((acc,curr) => 
+    (acc + (curr.quantity * curr.unitprice)) ,0)
+
+    return(
+        <div className='alert alert-primary'>
+            <span>Card value:{Location}{totalExpenses}</span>
+        </div>
+    )
 };
 
 export default CartValue;
