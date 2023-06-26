4de8932c93dccb26bd4cba405382e9b2369daac5
Kossi KOTOKLO <gessongue@gmail.com>
2023-06-26 20:49:25 +0100

    Implement ExpenseItem and ExpenseList components
    Signed-off-by: Kossi KOTOKLO <gessongue@gmail.com>

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,31 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { FaTimesCircle } from 'react-icons/fa';
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({id,name,quantity,unitprice}) => {
+
+    const {dispatch,Location} = useContext(AppContext)
+
+
+    const handleDeleteItem = () =>{
+
+        const item = {
+            name
+        }
+
+        dispatch({
+            type:"DELETE_ITEM",
+            payload:item
+        })
+    }
+    return (
+        <tr>
+              <th scope="row">{name}</th>
+              <td>{quantity}</td>
+              <td>{Location}{parseInt(unitprice)}</td>
+              <td>{Location}{parseInt(quantity)*parseInt(unitprice)}</td>
+              <td><FaTimesCircle onClick={handleDeleteItem} size='2.2rem' color='red'/></td>
+        </tr>
+    )
 };
 
 export default ExpenseItem;
